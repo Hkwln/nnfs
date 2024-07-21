14885695a1acd9c0955b16f2905a49ca0bcde3c7
Paulxocmd <sibila.paul2@gmail.com>
2024-07-21 11:00:30 +0200

    updated layers and testin github pull cause it my first time with vscode and github

---
+++ baselayerclass.py
@@ -0,0 +1,12 @@
+#Baser Layer
+
+class Layer:
+    def __init__(self) -> None:
+        self.input = None
+        self.output = None
+#computes the outpot Y of a layer for a given input X
+def forward_propagation(self,input):
+    raise NotImplementedError
+#computes dE/dX for a given dE/dY (and update parameters if any)
+def backward_propagation(self,output_error, learning_rate):
+    raise NotImplementedError
---
+++ fully-connected-layer.py
@@ -0,0 +1,6 @@
+from layer import Layer
+import numpy as np
+
+#inherit from base class Layer
+class FcLayer(Layer):
+    pass
