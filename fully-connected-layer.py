c79a92b405bd8716b61e34c52850b4574d91e7d8
Paulxocmd <sibila.paul2@gmail.com>
2024-07-21 12:19:24 +0200

    coding is fun when it just works

--- fully-connected-layer.py
+++ fully-connected-layer.py
@@ -1,6 +1,25 @@
-from layer import Layer
+from baselayerclass import Layer
 import numpy as np
 
 #inherit from base class Layer
 class FcLayer(Layer):
-    pass
+    #input_size = number of input neurons
+    #output_size = number of output neurons=
+    def __init__(self, input_size, output_size) -> None:
+        self.weights = np.random.ran(input_size,output_size) -0.5
+        self.bias = np.random.ran(1,output_size) -0.5
+    #returns outpot for a given input
+    def forward_propagation(self, input_data):
+        self.input = input_data
+        self.output = np.dot(self.input, self.weights) + self.bias
+        return self.output
+    #computes dE/dW, dE/dB for a fiven output_error = dE/dY. REturns input_errors=dE/dX.
+    def backward_propagation(self,output_error, learning_rate):
+        input_error = np.dot(self,output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error)
+        #dBias = output_error
+
+        #update paramaters
+        self.weights -= learning_rate * weights_error
+        self.bias -= learning_rate * output_error
+        return input_error
