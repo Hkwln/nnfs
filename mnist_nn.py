3d8a8b934d49843c24c281dadea62ad707b1eb67
Paulxocmd <sibila.paul2@gmail.com>
2024-07-22 14:22:15 +0200

    updated name and import project to mnist_nn

--- mnist_nn.py
+++ mnist_nn.py
@@ -1,4 +1,7 @@
 import numpy as np
 
 from network import Network
-from fully-connected-layer import FCLayer
+from fully_connected_layer import FCLayer
+from activation import tanh, tanh_prime
+from activation_layer import ActivationLayer
+from loss import mse, mse_prime
