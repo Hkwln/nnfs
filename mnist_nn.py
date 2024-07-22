857fbfd69d9af40f8defd7283d7bcdcc2a6eeed7
Paulxocmd <sibila.paul2@gmail.com>
2024-07-22 14:57:17 +0200

    :)

--- mnist_nn.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-from network import Network
-from fully_connected_layer import FCLayer
-from activation import tanh, tanh_prime
-from activation_layer import ActivationLayer
-from loss import mse, mse_prime
