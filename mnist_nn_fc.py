86da652ec4a1f4b9df3c64d6ac8f7c3f201022f2
hkwln <p.aul@mail.de>
2024-10-14 18:31:28 +0200

    reshaping the training data

--- mnist_nn_fc.py
+++ mnist_nn_fc.py
@@ -16,6 +16,27 @@ mnist_data =tfds.load("mnist")
 mnist_train, mnist_test = mnist_data["train"], mnist_data["test"]
 assert isinstance(mnist_train, tf.data.Dataset)
 print(mnist_data, mnist_train, mnist_test)
+#normalizing img
+#train
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+mnist_train = mnist_train.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+mnist_train = mnist_train.cache()
+mnist_train_train = mnist_train.shuffle(mnist_data.splits['train'].num_examples)
+mnist_train_train = mnist_train.batch(128)
+mnist_train_train = mnist_train.prefetch(tf.data.AUTOTUNE)
+  
+#test
+mnist_test =mnist_test.map(
+  normalize_img, num_parallel_calls=tf.data.AUTOTUNE
+)
+mnist_test = mnist_test.batch(128)
+mnist_test = mnist_test.cache()
+mnist_test = mnist_test.prefetch(tf.data.AUTOTUNE)
+
 #(x_train, y_train),(x_test, y_test) = mnist.load_data() keras
 
 # training data : 60000 samples
@@ -44,13 +65,13 @@ net.add(ActivationLayer(tanh, tanh_prime))
 
 # train on 1000 samples
 # as we didn't implemented mini-batch GD, training will be pretty slow if we update at each iteration on 60000 samples...
-#net.use(mse, mse_prime)
-#net.fit(x_train[0:1000], y_train[0:1000], epochs=35, learning_rate=0.1)
+net.use(mse, mse_prime)
+net.fit(mnist_train, epochs=35, learning_rate=0.1)
 
 # test on 3 samples
-#out = net.predict(x_test[0:3])
-#print("\n")
-#print("predicted values : ")
-#print(out, end="\n")
-#print("true values : ")
-#print(y_test[0:3])
+out = net.predict(mnist_test)
+print("\n")
+print("predicted values : ")
+print(out, end="\n")
+print("true values : ")
+print(mnist_test)
