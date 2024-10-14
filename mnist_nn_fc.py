7529eea8bcbf1856261463a5b92f8faee8ff981e
hkwln <p.aul@mail.de>
2024-10-14 12:41:59 +0200

    updated just a tiny error

--- mnist_nn_fc.py
+++ mnist_nn_fc.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from network import Network
-from fully_connected_layer import FCLayer
+from fully_connected_layer import FcLayer
 from activation import tanh, tanh_prime
 from activation_layer import ActivationLayer
 from  loss import mse, mse_prime
