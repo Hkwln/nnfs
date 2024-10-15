4f74f1792501e58da886b274041e1fef92303381
hkwln <p.aul@mail.de>
2024-10-15 17:46:08 +0200

    fixed np.random.ran() -> np.random.rand()

--- fully_connected_layer.py
+++ fully_connected_layer.py
@@ -6,8 +6,8 @@ class FcLayer(Layer):
     #input_size = number of input neurons
     #output_size = number of output neurons=
     def __init__(self, input_size, output_size) -> None:
-        self.weights = np.random.ran(input_size,output_size) -0.5
-        self.bias = np.random.ran(1,output_size) -0.5
+        self.weights = np.random.rand(input_size,output_size) -0.5
+        self.bias = np.random.rand(1,output_size) -0.5
     #returns outpot for a given input
     def forward_propagation(self, input_data):
         self.input = input_data
