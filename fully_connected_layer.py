3109bced198930c2d7ca58ed326700f0a0a1ba40
hkwln <p.aul@mail.de>
2024-10-30 16:10:05 +0100

    mini batch stochastic gradient descent

--- fully_connected_layer.py
+++ fully_connected_layer.py
@@ -6,12 +6,13 @@ class FcLayer(Layer):
     #input_size = number of input neurons
     #output_size = number of output neurons=
     def __init__(self, input_size, output_size, beta1=0.9, beta2 = 0.999, epsilon = 1e-8) -> None:
-        super().__init__()
+        super().__init__(self)
+        
         self.weights = np.random.randn(input_size,output_size) * 0.1
-        self.bias = np.random.rand(1,output_size) -0.5
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.epsilon = epsilon
+        self.bias = [np.random.randn(1,output_size)]
+        #self.beta1 = beta1
+        #self.beta2 = beta2
+        #self.epsilon = epsilon
        
     #returns outpot for a given input
     def forward_propagation(self, input_data):
@@ -21,6 +22,35 @@ class FcLayer(Layer):
         self.input = input_data
         self.output = np.dot(self.input, self.weights) + self.bias
         return self.output
+    #now i am trying to train the network using a mini-batch stochastic gradient descent
+    #The training data is a list of tubles with(image, label)
+    
+    def SGD(self, input_data, epochs, mini_batch_size, learning_rate, test_data=None):
+        n = len(input_data)
+        for j in range(epochs):
+            #time1 = time.time()
+            np.random.shuffle(input_data)
+            mini_batches = [input_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, learning_rate)
+            #time2 = time.time()
+            if test_data:
+                n_test = len(test_data)
+                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
+    #now we update the networks weights and biases
+    #mini batch is a list of tuples (x,y)
+    def update_mini_batch(self, mini_batch, learning_rate):
+        nabla_b = [np.zeros(b.shape) for b in self.bias]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(learning_rate/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.bias = [b-(learning_rate/len(mini_batch))*nb for b, nb in zip(self.bias, nabla_b)]
+
     #computes dE/dW, dE/dB for a fiven output_error = dE/dY. REturns input_errors=dE/dX.
     def backward_propagation(self,output_error, learning_rate):
         input_error = np.dot(output_error, self.weights.T)
