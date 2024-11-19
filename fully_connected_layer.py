correcting some shape errors and a bit of the mini-batch gradient algorithm

fully_connected_layer.py
@@ -17,6 +17,8 @@ class FcLayer(Layer):
     #returns outpot for a given input
     def forward_propagation(self, input_data):
         self.input = input_data
+        if self.input.ndim == 1:
+            self.input = self.input.reshape(-1, 1)
         self.output = np.dot(self.weights, self.input) + self.bias
         return self.output
     #now i am trying to train the network using a mini-batch stochastic gradient descent
@@ -53,53 +55,25 @@ class FcLayer(Layer):
     #they are representing the gradient for the cost function C_x 
     #nabla_b and nabla_w are layer-by-layer lists of numpy arrays, similar to self.bias and self.weights
     def backward_propagation(self, output_error, learning_rate):
-        nabla_b = [np.zeros(b.shape) for b in self.bias]
-        nabla_W = [np.zeros(w.shape) for w in self.weights]
+        if output_error.ndim == 1:
+            output_error = output_error.reshape(-1, 1)
+        input_error = np.dot(self.weights.T, output_error)
+        weights_error = np.dot(output_error, self.input.T)
         
-        # Initialize the activation list with the output_size
-        activation = [output_error]  # list to store all the activations, layer by layer
-        zs = []  # list to store all the z vectors, layer by layer
+        # Update parameters
+        self.weights -= learning_rate * weights_error
+        self.bias -= learning_rate * np.sum(output_error, axis=1, keepdims=True)
         
-        # Ensure that the activation list has at least one element before starting the loop
-        if len(activation) < 1:
-            raise IndexError("Activation list is empty, cannot proceed with backward propagation")
-        
-        # Feedforward
-        current_activation = output_error
-        for b, w in zip(self.bias, self.weights):
-            z = np.dot(w[+2], current_activation) + b
-            zs.append(z)
-            current_activation = self.sigmoid(z)
-            activation.append(current_activation)
-        
-        # Now the activation list should have enough elements
-        if len(activation) < 2:
-            raise IndexError("Not enough activations to access activation[-2]")
-        
-        # Backpropagation
-        delta = output_error * self.sigmoid_prime(zs[-1])
-        delta = delta.reshape(-1,1) #ensure delta is (output_size, 1)
-        activation[-1] = activation[-1].reshape(-1,1) #ensure activation is (output_size, 1)
-        nabla_b[-1] = delta
-        nabla_W[-1] = np.dot(delta, activation[-2])
-        
-        for l in range(2, self.num_layers):
-            z = zs[-l]
-            sp = self.sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-            nabla_b[-l] = delta
-            nabla_W[-l] = np.dot(delta, activation[-l-1].transpose())
-        
-        return nabla_b, nabla_W
+        return input_error
 
     #old code
-        #input_error = np.dot(output_error, self.weights.T)
-        #weights_error = np.dot(self.input.T, output_error)
-        #dBias = output_error
-        #update paramaters
-        #self.weights -= learning_rate * weights_error
-        #self.bias -= learning_rate * output_error
-        #return input_error
+    # def backward_propagation(self, output_error, learning_rate):
+    #     input_error = np.dot(output_error, self.weights.T)
+    #     weights_error = np.dot(self.input.T, output_error)
+    #     self.weights -= learning_rate * weights_error
+    #     self.bias -= learning_rate * output_error
+    #     return input_error
+
     def evaluate(self, test_data):
         test_results = [(np.argmax(self.forward_propagation(x)), y) for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)

loss.py
@@ -1,12 +1,18 @@
 import numpy as np
-#folgendes hat der debugger vorgeschlagen
-#following import is needed to use numpy functions in tensorflow
 from tensorflow.python.ops.numpy_ops import np_config
 np_config.enable_numpy_behavior()
 
-#loss function and its derivative
+# loss function and its derivative
 def mse(y_true, y_pred):
-    return np.mean(np.power(y_true-y_pred, 2))
+    if y_true.ndim == 1:
+        y_true = y_true.reshape(-1, 1)
+    if y_pred.ndim == 1:
+        y_pred = y_pred.reshape(-1, 1)
+    return np.mean(np.power(y_true - y_pred, 2))
 
 def mse_prime(y_true, y_pred):
-    return 2*(y_pred-y_true)/y_true.size
+    if y_true.ndim == 1:
+        y_true = y_true.reshape(-1, 1)
+    if y_pred.ndim == 1:
+        y_pred = y_pred.reshape(-1, 1)
+    return 2 * (y_pred - y_true) / y_true.size

mnist_nn_fc.py
@@ -1,3 +1,7 @@
+#set environment variable
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 import numpy as np
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
@@ -45,18 +49,13 @@ net.add(FcLayer(64, 10))                    # input_shape=(1, 50)       ;   outp
 net.add(ActivationLayer(tanh, tanh_prime))
 
 # train on 1000 samples
-# as we didn't implemented mini-batch GD, training will be pretty slow if we update at each iteration on 60000 samples...
-
-#trying out copilots suggestion
 for images, labels in mnist_train.take(1):
-    images = tf.reshape(images, (images.shape[0], -1)) #flatten the images
-    labels = tf.one_hot(labels, depth =10) #one hot encoding the labels
+    images = tf.reshape(images, (images.shape[0], -1)) # flatten the images
+    labels = tf.one_hot(labels, depth=10) # one hot encoding the labels
     images, labels = images.numpy(), labels.numpy()
     out = net.predict(images)
 net.use(mse, mse_prime)
-net.fit(images, labels , epochs=50, learning_rate=0.01)
-#net.loss = net.evaluate(images, labels)
-
+net.fit(images, labels, epochs=50, learning_rate=0.01)
 
 # test on 3 samples 
 #out = net.predict(images[:3])